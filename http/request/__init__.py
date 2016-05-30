162288f6be1a0be3a1eaba1aad344ae01e480af1
zhangchunyang <zhangchunyang_pri@126.com>
2016-05-30 22:59:47 +0800

    new Request

--- http/request/__init__.py
+++ http/request/__init__.py
@@ -1,5 +1,7 @@
 #!coding:utf8
-
+import sys
+sys.path.append('/home/zhangchunyang/Araneae')
+print sys.path
 
 import time
 import urllib
@@ -10,7 +12,7 @@ import Araneae.utils.http as UTLH
 import Araneae.man.exception as EXP
 from Araneae.compat import json as Json
 from Araneae.utils.livetracker import LiveObject
-from Araneae,utils.url import safe_url_string
+from Araneae.utils.url import (safe_url_string,escape_ajax)
 
 DEFAULT_REQUEST_METHOD = 'GET'
 DEFAULT_REQUEST_TIMEOUT = 2
@@ -18,44 +20,34 @@ DEFAULT_CALLBACK = 'parse'
 DEFAULT_ASSOCIATE = False
 
 class Request(LiveObject):
-    """
-    spider_name:爬虫名
-    url:统一资源定位符
-    method:方式
-    headers:头信息
-    data:提交数据
-    cookies:cookie信息
-    callback:回调函数对象
-    auth:认证
-    proxies:代理
-    associate:是否关联
-    """
 
-    def __init__(self, url, callback = None, method = DEFAULT_REQUEST_METHOD, encoding = 'utf8', proxies = None,associate = False,
-                 priority = 0, headers = None, data = None, params = None,auth = None,
-                 cookies = None,json = None,dont_filter = None, errback = None):
+    def __init__(self, url, callback = None, method = DEFAULT_REQUEST_METHOD, encoding = 'utf8', proxies = None,
+                 headers = None, data = None, params = None,auth = None,cookies = None,
+                 json = None,dont_filter = False, errback = None):
         self._encoding = encoding
         self.method = str(method).upper()
         self._set_url(url)
-        self.__url = UTLH.revise_url(url)
+        self._data = data
+        assert isinstance(priority,int), 'Request优先级必须是int' % priority
+        self._priority = priority
+        self._proxies = proxies or {}
+        self._params = params or {}
+        self._json = json or {}
+        self._auth = auth or {}
 
-        self.__method = UTLH.validate_method(args.get('method', 'GET'))
-        self.__headers = args.get('headers', {})
-        self.__data = args.get('data', {})
-        self.__cookies = args.get('cookies', {})
-        self.__auth = args.get('auth', {})
-        self.__proxies = args.get('proxies', None)
+        assert callback or not errback, 'Request不能在没有callback的情况下使用errback'
+        self.callback = callback
+        self.errback = errback
 
-        self.__spider_name = args.get('spider_name', '')
-        self.__fid = args.get('fid', None)
-        self.__rule_number = args.get('rule_number', None)
-        self.__callback = args.get('callback', DEFAULT_CALLBACK)
-        self.__associate = args.get('associate', DEFAULT_ASSOCIATE)
+        self.headers = headers or {}
+        self.cookies = cookies or {}
+        
+        self.dont_filter = dont_filter
 
-        #  为了方便使用 '->'.join() 方法连接字符串，无法以 tuple 的形式存储到 list 中
-        #  因此，只能把 url_route(list) 和 title_route(list) 分别存储
-        self.__url_route = args.get('url_route', [])
-        self.__title_route = args.get('title_route', [])
+    def __str__(self):
+        return '<%s %s>' % (self.method,self._url)
+
+    __repr__ = __str__
 
     def _set_url(self,url):
         if not isinstance(url, six.string_types):
@@ -65,200 +57,37 @@ class Request(LiveObject):
         #如果网站支持,获取网页的snapshot
         self._url = escape_ajax(safe_url)
 
-        if ':' no tin self._url:
+        #如果网址没有schema,自动补全为http://
+        if ':' not in self._url:
             self._url = autocomplete_url_schema(self._url)
-        
 
-    def _sequence_json(self):
-        request_json = {}
-        request_json['url'] = self.__url
+    def copy(self):
+        return self.replace()
 
-        if self.__spider_name:
-            request_json['spider_name'] = self.__spider_name
+    def to_json(self):
+        return {'url':self._url,'method':self.method,'encoding':self._encoding,'callback':self.callback,
+                'proxies':self._proxies,'headers':self.headers,'data':self._data,'params':self._params,
+                'auth':self._auth,'cookies':self.cookies,'json':self._json,
+                'dont_filter':self.dont_filter,'errback':self.errback}
 
-        if self.__method:
-            request_json['method'] = self.__method
-        if self.__headers:
-            request_json['headers'] = self.__headers
-        if self.__data:
-            request_json['data'] = self.__data
-        if self.__cookies:
-            request_json['cookies'] = self.__cookies
-        if self.__auth:
-            request_json['auth'] = self.__auth
-        if self.__proxies:
-            request_json['proxies'] = self.__proxies
+    def to_json_string(self):
+        return Json.dumps(self.to_json(),ensure_ascii = True)
 
-        if self.__callback:
-            request_json['callback'] = self.__callback
-        if self.__fid:
-            request_json['fid'] = self.__fid
+    @classmethod
+    def replace(cls, *args, **kwargs):
+        for x in ['url','callback','method','encoding','proxies','priority','headers',
+                  'data','params','auth','cookies','json','dont_filter','errback']:
+            kwargs.setdefault(x, getattr(self,x))
+            return cls(*args, **kwargs)
 
-        if not self.__rule_number:
-            raise EXP.RequestException('request对象必须设置rule number')
-
-        request_json['rule_number'] = self.__rule_number
-
-        request_json['associate'] = self.__associate
-
-        request_json['url_route'] = self.__url_route[:]
-        request_json['title_route'] = self.__title_route[:]
-
-        return json.dumps(request_json, ensure_ascii=False)
-
-    def set_spider_name(self, spider_name):
-        self.__spider_name = spider_name
-        return self
-
-    def set_rule_number(self, rule_number):
-        self.__rule_number = rule_number
-        return self
-
-    def set_fid(self, fid):
-        self.__fid = fid
-        return self
-
-    def set_auth(self, auth):
-        self.__auth = auth
-        return self
-
-    def set_user_agent(self, user_agent):
-        self.__headers['User-Agent'] = user_agent
-        return self
-
-    def set_http_proxy(self,proxy):
-        self.__proxies = {'http':proxy}
-        return self
-
-    def set_https_proxy(self,proxy):
-        self.__proxies = {'https':proxy}
-        return self
-
-    def set_associate(self,associate):
-        self.__associate = associate
-        return self
-
-    def add_headers(self,header_dict):
-        self.__headers = dict(self._headers,**header_dict)
-        return self
-
-    def set_headers(self, header_dict):
-        self.__headers = header_dict
-        return self
-
-    def add_cookies(self, cookie_dict):
-        self.__cookies = dict(self._cookies, **cookie_dict)
-
-    #  为了方便使用 '->'.join() 方法连接字符串
-    #  此处把 url_list 和 title_list 分别存储
-    def add_url_route_element(self, route_element):
-        self.__url_route.append(route_element[0])
-        self.__title_route.append(route_element[1])
-        return self
-
-    def fetch(self, timeout=DEFAULT_REQUEST_TIMEOUT):
-        """
-        抓取页面信息
-        """
-        try:
-            response = getattr(requests, self.__method)(self.__url,
-                                                        proxies=self.__proxies,
-                                                        data=self.__data,
-                                                        headers=self.__headers,
-                                                        cookies=self.__cookies,
-                                                        timeout=timeout)
-        except requests.exceptions.ConnectionError:
-            raise EXP.RequestConnectionException('DNS查询失败或者拒绝连接')
-        except requests.exceptions.HTTPError:
-            raise EXP.RequestErrorException('无效HTTP响应')
-        except requests.exceptions.Timeout:
-            raise EXP.RequestTimeoutException('请求超时')
-        except requests.exceptions.TooManyRedirects:
-            raise EXP.RequestTooManyRedirectsException('超过最大重定向次数')
-
-        return response
+    @property
+    def encoding(self):
+        return self._encoding
 
     @property
     def url(self):
-        return self.__url
+        return self._url
 
-    @property
-    def method(self):
-        return self.__method
 
-    @property
-    def rule_number(self):
-        return self.__rule_number
-
-    @rule_number.setter
-    def rule_number(self, rule_number):
-        self.__rule_number = rule_number
-
-    @property
-    def headers(self):
-        return self.__headers
-
-    @property
-    def cookies(self):
-        return self.__cookies
-
-    @property
-    def data(self):
-        return self.__data
-
-    @property
-    def callback(self):
-        return self.__callback
-
-    @callback.setter
-    def callback(self, callback):
-        self.__callback = callback
-
-    @property
-    def json(self):
-        return self._sequence_json()
-
-    @property
-    def fid(self):
-        return self.__fid
-
-    @fid.setter
-    def fid(self, fid):
-        self.__fid = fid
-
-    @property
-    def associate(self):
-        return self.__associate
-
-    @associate.setter
-    def associate(self, associate):
-        self.__associate = associate
-
-    @property
-    def url_route(self):
-        #  返回拷贝，不是引用
-        return self.__url_route[:]
-
-    @property
-    def title_route(self):
-        return self.__title_route[:]
-
-    def get_title(self, ind = -1):
-
-        lastIndex = len(self.__title_route) - 1
-
-        if lastIndex >= 0 and ind < lastIndex:
-            if ind == -1:
-                return self.__title_route[lastIndex]
-            else:
-                return self.__title_route[ind]
-        else:
-            return "None"
-
-def json2request(request_json):
-    """
-    json串转换成request对象
-    """
-    request_json = json.loads(request_json)
-    request = Request(**request_json)
-    return request
+if __name__ == '__main__':
+    pass
