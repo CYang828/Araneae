f4b792e9f9484d02483e591347dabfc28fc1216f
zhangchunyang <zhangchunyang@kdq-e01-02-01-vm-back-test-reg-100.(none)>
2016-06-21 18:13:34 +0800

    add

--- constant.py
+++ constant.py
@@ -4,6 +4,9 @@
 """
 常量
 """
+import re
+
+
 #调度器默认拉取超时时间
 DEFAULT_SCHEDULER_PULL_TIMEOUT = 5
 #调度器默认拉取条数
@@ -12,8 +15,12 @@ DEFAULT_SCHEDULER_PULL_COUNT = 1
 #配置文件中规则前缀
 RULE_PREFIX = 'PAGE'
 
-#叶子配置文件中规则前缀
-LEAF_PREFIX = 'LEAF'
+#树状结构数据子节点配置文件中规则前缀
+TREE_DATA_CHILD_PREFIX = 'CHILD'
+
+#树状结构数据匹配上级正则
+TREE_DATA_PARENT_REGEX = re.compile(r'!PAR')
+
 
 #默认request方法
 DEFAULT_REQUEST_METHOD = 'GET'
--- extractors/data.py
+++ extractors/data.py
@@ -2,7 +2,7 @@
 
 from collections import (OrderedDict,defaultdict)
 
-from Araneae.constant import LEAF_PREFIX
+from Araneae.constant import (TREE_DATA_CHILD_PREFIX,TREE_DATA_PARENT_REGEX)
 from Araneae.uitls.python import arg_to_iter
 from Araneae.utils.extractor import response_to_selector
 
@@ -11,13 +11,13 @@ class DataExtractor(object):
     """数据抽取器"""
 
     def __init__(self, regulations=None):
-        self._leaf_prefix_len = len(LEAF_PREFIX)
+        self._child_prefix_len = len(TREE_DATA_CHILD_PREFIX)
         self.regulations = arg_to_iter(regulations)
 
     def get_selector(self, selector, rgl):
         assert rgl.get('type'), 'Extract data regulation must have `type` key'
         assert rgl.get('expression'), 'Extract data regulation must have `expression` key'
-        
+
         t = rgl.get('type')
         e = rgl.get('expression')
 
@@ -25,63 +25,97 @@ class DataExtractor(object):
                 'css':lambda s,e: s.xpath(e),
                 're':lambda sme: s.xpath(e)}[t](selector, e)
 
-    def get_raw_data(self, selector, regulation, recursion=False):
-        """返回的raw data都是iter"""
+
+    def extract_raw_data(self, response, regulation, recursion=False):
+        """返回的raw data都是iter
+        raw data格式[{field:data},{},...]"""
+
+        selector = response_to_selector(response)
 
         assert regulation.get('type'), 'Extract data regulation must have `type` key'
-        assert regulation.get('expression'), 'Extract data regulation must have `expression` key'
         
         t = regulation.get('type')
-        e = regulation.get('expression')
-        f = regulation.get('field')
         raw_data = []
 
         if t == 'func':
-            raw_data = self._func_parse(expression)
+            raw_data = self._func_parse(regulation)
         elif t == 'tree' and not recursion:
             raw_data = self._tree_parse(selector, regulation)
         elif t == 'table' and not recursion:
             pass
         else:
-            raw_data = get_selector(selector, t, e).extract()
+            raw_data = self._sel_parse(selector, regulation)
 
         return raw_data
 
-    def _func_parse(self, expression):
+    def _sel_parse(self, selector, rgl):
+        assert rgl.get('field'), 'Extract data regulation must have `field` key'
+        f = rgl.get('field')
+        m = rgl.get('mulrecord', False)
+
+        data = self.get_selector(selector, rgl).extract()
+
+        return [{f:d} for d in data] if m else [].append({f:data})
+
+    def _func_parse(self, rgl):
+        assert rgl.get('expression'), 'Extract data regulation must have `expression` key'
+        assert rgl.get('field'), 'Extract data regulation must have `field` key'
+        e = rgl.get('expression')
+        f = rgl.get('field')
+        m = rgl.get('mulrecord', False)
+
         data = []
 
-        if isinstance(expression, six.string_types):
+        if isinstance(e, six.string_types):
             data = arg_to_iter(eval(expression))
         elif hasattr(e, '__call__'):
-            data = arg_to_iter(expression())
+            data = arg_to_iter(e())
         else isinstance(e, list):
-            data = arg_to_iter(expression)
-
-        return data
+            data = arg_to_iter(e)
+        
+        return [{f:d} for d in data] if m else [].append({f:data})
 
     def _tree_parse(self, selector, regulation):
         assert regulation.get('root'), 'Extract tree data must have `root` key'
         root = regulation.get('root')
 
-        leafs = {k:v for k,v in regulation.iteritems() if key.startswith(LEAF_PREFIX)]
-        assert len(leafs), 'Extract tree data must have `{}` prefix key'.format(LEAF_PREFIX)
+        childs = {k:v for k,v in regulation.iteritems() if key.startswith(CHILD_PREFIX)]
+        assert len(leafs), 'Extract tree data must have `{}` prefix key'.format(TREE_DATA_CHILD_PREFIX)
         
-        leafs = OrderedDict(sorted(leafs.items(), key=lambda x:int(x[0][self._leaf_prefix_len():])))
+        childs = OrderedDict(sorted(childs.items(), key=lambda x:int(x[0][self._child_prefix_len():])))
         root = self.get_selector(selector, root)
 
         assert len(root), 'Extract tree root just have only one'
 
-        expression_register = defaultdict()
-        result_register = defaultdict()
-        
-        for k,v in leafs.iteritems():
-            assert regulation.get('expression'), 'Extract tree data must have `expression` key'
-            
-            #根据正则,生成对应关系的xpath
-            expression_register[k] = v['expression']
-            result_register[k] = self.get_selector(selector, v))
-             
+        expression_register = OrderedDict()
+        result_register = defaultdict(list)
+        field_register = OrderedDict()
 
+        for k,v in leafs.iteritems():
+            assert v.get('expression'), 'Extract tree data must have `expression` key'
+            assert v.get('field'), 'Extract tree data must have `field` key'
+
+            e = arg_to_iter(v.get('expression'))
+            f = v.get('field')
+            uk = leafs.keys().index(k)-1
+        
+            if uk >= 0:
+                e = [TREE_DATA_PARENT_REGEX.sub(c, e) for c in range(len(result_register[uk]))]
+            
+            for me in e:
+                vm = v
+                vm['expression'] = me
+                rm = get_selector(selector, vm)
+                result_register[k].append(rm.extract())              
+            
+            expression_register[k] = e
+            field_register[k] = f
+
+       
+
+        
+        
+        
     def _table_parse(self, selector, regulation):
         pass
 
